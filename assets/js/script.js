63054d3078575c55fc1b434c00d87b3a6e557af2
Jonelle Adona <jonelle.adona@knights.ucf.edu>
2020-10-14 02:46:31 -0400

    Add time tags to schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,41 @@
-var timeBlockRow = $("<div/>", {
-  class: "row",
-  id: "timeBlockRow"
-}).appendTo("#timeBlockContainer");
 
-var timeBlockHour = $("<div/>", {
-  class: "col",
-  id: "hour"
-});
+var businessHrs = 9;
+var arrayHours = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
 
-var timeBlockEvent = $("<div/>", {
-  class: "col",
-  id: "event"
-});
 
-var timeBlockSave = $("<div/>", {
-  class: "col",
-  id: "save"
-});
+// Creats the timeblock rows and columns
+for (var i = 0; i < businessHrs; ++i)
+{
+  var timeBlockRow = $("<div/>", {
+    class: "row",
+    id: "timeBlockRow" + i
+  }).appendTo("#timeBlockContainer");
 
-// Time block container
-$("#timeBlockRow").append(timeBlockHour, timeBlockEvent, timeBlockSave); 
+  var timeBlockHour = $("<div/>", {
+    class: "col",
+    id: "hour" + i
+  });
+
+  var timeBlockEvent = $("<div/>", {
+    class: "col",
+    id: "event" + i
+  });
+
+  var timeBlockSave = $("<div/>", {
+    class: "col",
+    id: "save" + i
+  });
+
+  // Time block container
+  $("#timeBlockRow" + i).append(timeBlockHour, timeBlockEvent, timeBlockSave); 
+
+  // Accesses array
+  
+  var hourContent = document.createTextNode(arrayHours[i]);
+  $("#hour" + i).append(hourContent);
+
+}
+
+
+
+
